cd8afeb133b83d5c98bf2b3f5371df72f64dd807
Zach T <ztrokey@gmail.com>
2021-04-15 09:13:37 -0600

    add exists method and test to ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,1 +1,8 @@
-#
+class Ride
+  attr_reader :name, :cost
+
+  def initialize(details)
+    @name = details[:name]
+    @cost = details[:cost]
+  end
+end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -8,4 +8,32 @@ RSpec.describe Ride do
 
     expect(ride).to be_instance_of(Ride)
   end
-end 
+end
+
+
+
+#
+# pry(main)> ride.name
+# #=> "Ferris Wheel"
+#
+# pry(main)> ride.cost
+# #=> 0
+#
+# pry(main)> attendee = Attendee.new('Bob', 20)
+# #=> #<Attendee:0x00007ff88ea979a8...>
+#
+# pry(main)> attendee.name
+# #=> "Bob"
+#
+# pry(main)> attendee.spending_money
+# #=> 20
+#
+# pry(main)> attendee.interests
+# #=> []
+#
+# pry(main)> attendee.add_interest('Bumper Cars')
+#
+# pry(main)> attendee.add_interest('Ferris Wheel')
+#
+# pry(main)> attendee.interests
+# #=> ["Bumper Cars", "Ferris Wheel"]
