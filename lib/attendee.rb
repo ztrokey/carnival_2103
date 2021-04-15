188020cb48c0b7a9003500e525d7cd151480e63b
Zach T <ztrokey@gmail.com>
2021-04-15 09:22:23 -0600

    add interest attribute to attendee class

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,8 +1,9 @@
 class Attendee
-  attr_reader :name, :spending_money
-  
+  attr_reader :name, :spending_money, :interests
+
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
+    @interests = []
   end
 end
