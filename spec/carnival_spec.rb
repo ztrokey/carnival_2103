4128f5c56f92d2b19b8022a6d16f0b02ac4f05ca
Zach T <ztrokey@gmail.com>
2021-04-15 09:41:25 -0600

    Add recommended rides tests to carnival spec

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -28,23 +28,22 @@ RSpec.describe Carnival do
 
     expect(jeffco_fair.rides).to eq([ferris_wheel, bumper_cars, scrambler])
   end
-end
 
-#
-# pry(main)> bob = Attendee.new('Bob', 20)
-# #=> #<Attendee:0x00007fe59507dc80...>
-#
-# pry(main)> sally = Attendee.new('Sally', 20)
-# #=> #<Attendee:0x00007fe593ac3318...>
-#
-# pry(main)> bob.add_interest('Ferris Wheel')
-#
-# pry(main)> bob.add_interest('Bumper Cars')
-#
-# pry(main)> sally.add_interest('Scrambler')
-#
-# pry(main)> jeffco_fair.recommend_rides(bob)
-# #=> [#<Ride:0x00007fe59506ede8...>, #<Ride:0x00007fe594851930...>]
-#
-# pry(main)> jeffco_fair.recommend_rides(sally)
-# #=> [#<Ride:0x00007fe59314eb98...>]
+  it 'can recommend rides to attendees' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+    ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+    bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+    scrambler = Ride.new({name: 'Scrambler', cost: 15})
+    jeffco_fair.add_ride(ferris_wheel)
+    jeffco_fair.add_ride(bumper_cars)
+    jeffco_fair.add_ride(scrambler)
+    bob = Attendee.new('Bob', 20)
+    sally = Attendee.new('Sally', 20)
+    bob.add_interest('Ferris Wheel')
+    bob.add_interest('Bumper Cars')
+    sally.add_interest('Scrambler')
+
+    expect(jeffco_fair.recommend_rides(bob)).to eq([ferris_wheel, bumper_cars])
+    expect(jeffco_fair.recommend_rides(sally)).to eq([scrambler])
+  end
+end
