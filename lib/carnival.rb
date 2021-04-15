31d3c26456d40bf005259f9640693ec6c1965113
Zach T <ztrokey@gmail.com>
2021-04-15 09:37:49 -0600

    add add_ride method to carnival class

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,8 +1,12 @@
 class Carnival
-  attr_reader :name, :rides 
+  attr_reader :name, :rides
 
   def initialize(name)
     @name = name
     @rides = []
+  end
+
+  def add_ride(ride)
+    rides << ride
   end
 end
