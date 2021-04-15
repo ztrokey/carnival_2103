6e1d0d7cbd0c23fdac9847e078e258e8b48d5a02
Zach T <ztrokey@gmail.com>
2021-04-15 09:08:25 -0600

    Add tests for ride and attendee

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -1,1 +1,11 @@
-#
+require 'rspec'
+require './lib/ride'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  it 'exists' do
+    attendee = Attendee.new('Bob', 20)
+
+    expect(attendee).to be_instance_of(Attendee)
+  end
+end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,1 +1,11 @@
-#
+require 'rspec'
+require './lib/ride'
+require './lib/attendee'
+
+RSpec.describe Ride do
+  it 'exists' do
+    ride = Ride.new({name: 'Ferris Wheel', cost: 0})
+
+    expect(ride).to be_instance_of(Ride)
+  end
+end 
