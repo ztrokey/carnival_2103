77bedf30c11c5305df921fc8437e6c97a61212ef
Zach T <ztrokey@gmail.com>
2021-04-15 10:04:06 -0600

    add recommened rides method to carnival class

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -9,4 +9,16 @@ class Carnival
   def add_ride(ride)
     rides << ride
   end
+
+  def recommend_rides(attendee)
+    recommended_rides = []
+    rides.each do |ride|
+      attendee.interests.each do |interest|
+        if ride.name == interest
+          recommended_rides << ride
+        end
+      end
+    end
+    recommended_rides
+  end
 end
