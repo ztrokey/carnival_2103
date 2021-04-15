6c92b04395b2601d2d7949716ca7e748c084fd76
Zach T <ztrokey@gmail.com>
2021-04-15 10:10:03 -0600

    add attendees array to carnival class

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,9 +1,10 @@
 class Carnival
-  attr_reader :name, :rides
+  attr_reader :name, :rides, :attendees 
 
   def initialize(name)
     @name = name
     @rides = []
+    @attendees = []
   end
 
   def add_ride(ride)
